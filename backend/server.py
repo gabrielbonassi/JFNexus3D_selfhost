bc2b618111ca77582013e6ca2966572cc2c0f0c2
gabrielbonassi <gabrielfaganellobonassi@gmail.com>
2026-05-23 15:05:06 -0300

    deploy back e front

--- backend/server.py
+++ backend/server.py
@@ -58,11 +58,14 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://jf-nexus3-d-selfhost.vercel.app"
     ],
     allow_origin_regex=(
         r"^https://.*\.app\.github\.dev$|"
-        r"^https://.*\.devtunnels\.ms$"
+        r"^https://.*\.devtunnels\.ms$|"
+        r"^https://.*\.vercel\.app$"
     ),
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
